b94dccac31dc80a96db7643dba50b64a3755627b
alvinsanchez93 <159292482+alvinsanchez93@users.noreply.github.com>
2026-02-03 09:54:39 -0800

    feat: implement complete command with partial ID matching

--- TaskMaster.CLI/Program.cs
+++ TaskMaster.CLI/Program.cs
@@ -31,7 +31,7 @@ while(true)
             repository.AddTask(newTask);
 
             Console.WriteLine("Task added sucessfully!");
-        break;
+            break;
 
         case "list":
             var tasks = repository.GetAllTasks();
@@ -42,8 +42,23 @@ while(true)
             }
             break;
 
+        case "complete":
+            Console.Write("Enter Task  ID (or first few character): ");
+            string idInput = Console.ReadLine() ?? "";
+
+            if (repository.UpdateTaskStatus(idInput, TaskMaster.CLI.Models.TaskStatus.Completed))
+            {
+                Console.WriteLine("Task marked as Completed!");
+            }
+            else
+            {
+                Console.WriteLine("Task not found with that ID.");
+            }
+            break;
+
         default:
             Console.WriteLine("Unknow command. Try: add, list, or exit.");
             break;
+
     }
 }
